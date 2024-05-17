a9849c3947d14bbf04b55e0530ce4e1989c2fe64
Dev Kumar <110048121+DevKumar57-67@users.noreply.github.com>
2024-05-17 20:53:34 +0530

    Create Documentations.py

---
+++ Documentations.py
@@ -0,0 +1,16 @@
+INTRO OF THE MODEL:
+
+
+The Memogpt model generates funny and effective templates for your memes.If you are confused to prefer which template to use you can simply give.your prompt and within seconds you will get your template and a funny text for your memes.
+If you give a situation  to this bot without much reference then too the bot will give you some templates for memes.
+Remember this bot is still in development so it can make mistakes.But upvoming updates will enhance it's performance.
+
+HOW TO USE:
+
+You simple need to give it a prompt and you will get your template within seconds.
+
+Hope you will Like it
+
+Thanks!
+
+From the Memogpt developer!
